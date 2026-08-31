0223fd874200f3960f9fdb19a7ac7ca8b7f25115
Gnarkill33 <gnarkill33@gmail.com>
2026-08-31 23:28:50 +0300

    fix: check different versions

--- container/src/App.js
+++ container/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import MarketingApp from "./components/MarketingApp";
+import MarketingRootApp from "./components/MarketingRootApp";
 
 export default () => {
- return <MarketingApp />;
+ return <MarketingRootApp />;
 };
