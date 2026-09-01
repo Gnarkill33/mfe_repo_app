6d8de844462b95d209c92411e228593dc3d51655
Gnarkill33 <gnarkill33@gmail.com>
2026-09-01 23:32:25 +0300

    feat: add header to container

--- container/src/App.js
+++ container/src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingRootApp from "./components/MarketingRootApp";
+import Header from "./components/Header";
 
 export default () => {
- return <MarketingRootApp />;
+ return (
+  <BrowserRouter>
+   <div>
+    <Header />
+    <MarketingRootApp />
+   </div>
+  </BrowserRouter>
+ );
 };
