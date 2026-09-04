8a4295c914b8710558673067a41a12aec424b1ce
Gnarkill33 <gnarkill33@gmail.com>
2026-09-05 00:02:03 +0300

    feat: add changes to container to include dashboard

--- container/src/App.js
+++ container/src/App.js
@@ -1,16 +1,26 @@
-import React, { Suspense, lazy, useState } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { Suspense, lazy, useState, useEffect } from "react";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import Header from "./components/Header";
 import Progress from "./components/Progress";
+import { createBrowserHistory } from "history";
 
 const AuthLazy = lazy(() => import("./components/AuthApp"));
 const MarketingLazy = lazy(() => import("./components/MarketingRootApp"));
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
+
+const history = createBrowserHistory();
 
 export default () => {
  const [isSignedIn, setIsSignedIn] = useState(false);
 
+ useEffect(() => {
+  if (isSignedIn) {
+   history.push("/dashboard");
+  }
+ }, [isSignedIn]);
+
  return (
-  <BrowserRouter>
+  <Router history={history}>
    <div>
     <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
     <Suspense fallback={<Progress />}>
@@ -18,10 +28,14 @@ export default () => {
       <Route path='/auth'>
        <AuthLazy onSignIn={() => setIsSignedIn(true)} />
       </Route>
+      <Route path='/dashboard'>
+       {!isSignedIn && <Redirect to='/' />}
+       <DashboardLazy />
+      </Route>
       <Route path='/' component={MarketingLazy} />
      </Switch>
     </Suspense>
    </div>
-  </BrowserRouter>
+  </Router>
  );
 };
